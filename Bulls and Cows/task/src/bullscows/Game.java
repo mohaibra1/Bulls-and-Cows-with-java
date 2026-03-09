aea293b46e1bc098de299928a9e3a39771c09f0d
mohaibra <mimohadinho@gmail.com>
2026-03-10 00:58:00 +0100

    stage 6/7

--- Bulls and Cows/task/src/bullscows/Game.java
+++ Bulls and Cows/task/src/bullscows/Game.java
@@ -1,5 +1,6 @@
 package bullscows;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -8,13 +9,19 @@ public class Game {
     public void start() {
         Scanner input = new Scanner(System.in);
         System.out.println("Please, enter the secret code's length:");
-        //user input
 
+        //user input
         int n = input.nextInt();
+        System.out.println("Input the number of possible symbols in the code:");
+        int strLength = input.nextInt();
         while(true){
-            if(n > 10){
+            if(n > 37){
                 System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
                 n = input.nextInt();
+                continue;
+            }else if(strLength > 37) {
+                System.out.println("String length should be less than 26 or equal. not Negative!");
+                strLength = input.nextInt();
                 continue;
             }else {
                 break;
@@ -24,11 +31,15 @@ public class Game {
         boolean stopGame = true;
         int turns = 1;
         // generate once the secretcode.
-        String secretCode = generateSecretCode(n);
+        String secretCode = generateSecretCode(n, strLength);
         while(stopGame) {
 
             System.out.println("Turn " +  turns++ + ":");
             String userInput = input.next();
+            if(userInput.length() > n){
+                System.out.println("Put " + n + " Numbers!");
+                continue;
+            }
 
             int[] arr = checkBullsAndCows(userInput, secretCode);
             int bulls = arr[0];
@@ -68,29 +79,60 @@ public class Game {
         return answer;
     }
 
-    private String generateSecretCode(int n){
+    private String generateSecretCode(int n, int strLength) {
+        Random random = new Random();
         StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        while(sb2.length() < n){
-            long random = System.nanoTime();
-            sb.append(random);
-            sb.reverse();
-            char[] arr = sb.toString().toCharArray();
-
-            for(int i = 0; i < arr.length; i++){
-                if(sb2.length() != n){
-                    if(sb2.isEmpty() && arr[i] == '0'){
-                        continue;
-                    }
-                    if(!sb2.toString().contains(String.valueOf(arr[i]))){
-                        sb2.append(arr[i]);
-                    }
-                }
+        char[] chars = getRandom(n, strLength);
+        while (sb.length() < n) {
+            String num;
+            if(strLength == 0){
+                num = String.valueOf(random.nextInt(10));
+            }else {
+                num = String.valueOf(chars[random.nextInt(chars.length)]);
             }
-            sb.setLength(0);
+            if (sb.toString().contains(num)) {
+                continue;
+            }
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+
+    private char[] getRandom(int n, int strLength){
+        char[] letters = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
+                ,'q','r','s','t','u','v','w','x','y','z'};
+        StringBuilder sb = new StringBuilder();
+        String star = "";
+        for(int i = 0; i < n; i++){
+            star += "*";
         }
 
-        //System.out.println("The random secret number is " + sb2.toString() + ".");
-        return sb2.toString();
+        int numberOfSymbols = strLength + n;
+        int index = 0;
+        while(index < strLength){
+            sb.append(letters[index]);
+            index++;
+        }
+        StringBuilder numbers = new StringBuilder();
+        StringBuilder chars = new StringBuilder();
+        for(int i = 0; i < sb.length(); i++){
+            if(Character.isDigit(sb.charAt(i))){
+                numbers.append(sb.charAt(i));
+            }else {
+                chars.append(sb.charAt(i));
+            }
+        }if(strLength <= 10){
+            System.out.println("The secret is prepared: " +
+                    star + " ("  + numbers.charAt(0) + "-" +
+                    numbers.charAt(numbers.length()-1) + ")");
+        }else {
+
+            System.out.println("The secret is prepared: " +
+                    star + " (" + numbers.charAt(0) + "-" +
+                    numbers.charAt(numbers.length() - 1) + ", " + chars.charAt(0) + "-" +
+                    chars.charAt(chars.length() - 1) + ")");
+        }
+        System.out.println(sb);
+        return sb.toString().toCharArray();
     }
 }
